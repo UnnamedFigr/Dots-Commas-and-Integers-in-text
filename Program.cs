8538a74b62460854c0f49be025c9e04cb4cd6689
UnnamedFigr <121351631+UnnamedFigr@users.noreply.github.com>
2023-01-10 17:48:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Enumeration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter file name:");
+        string fileName = Console.ReadLine();
+        string minimumLetters = "";
+
+        using (StreamReader reader = new StreamReader($"{fileName}.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                minimumLetters += new string(line.Distinct().ToArray());
+            }
+        }
+
+        Console.WriteLine(minimumLetters);
+    }
+}
